9eb3684d3fd81e1bd1fd55d5326098fdfe45769e
Michelle <jjuanpedro491@gmail.com>
2022-05-16 21:34:50 -0500

    apiapi

--- scripts/api.js
+++ scripts/api.js
@@ -1,13 +1,15 @@
 const contenedor = document.getElementById("contenido-principal");
-const url = "https://raw.githubusercontent.com/MarioMarinDev/Grupo-1-API/master/base_datos.json";
-const uri = "https://raw.githubusercontent.com/MarioMarinDev/Grupo-1-API/master/";
+const url = "https://raw.githubusercontent.com/MichMejia/Base-API/master/base_datos.json";
+const uri = "https://raw.githubusercontent.com/MichMejia/Base-API/master/";
 
 fetch(url).then(function(respuesta) {
   return respuesta.json();
 }).then(function(datos) {
   console.log(datos);
   for(let i = 0; i < datos.length; i++) {
-    contenedor.innerHTML += "<div class='poster'><img src='" + uri + datos[i]["imagen"] + "'><h3>" + datos[i]["nombre"] + "</h3></div>";
+    contenedor.innerHTML += "<div class='poster'>";
+    contenedor.innerHTML += "<img src='" + uri + datos[i]["imagen"] + "'>"
+    contenedor.innerHTML += "<h3>" + datos[i]["Tipo"] + "</h3></div>";
   }
 }).catch(function(error) {
   console.log(error);
